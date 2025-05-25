f892ddface294e84fa3b8ccd6fdf6e2f1c5b8df8
CalzadaF <a21490530@itmexicali.edu.mx>
2025-05-25 15:14:53 -0700

    problema puertoooo

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from app import create_app
 import os
 from flask import Flask
+from app import create_app
 
 app= create_app()
 
-print("Puerto asignado por Render:", os.environ.get("PORT"))
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Toma el puerto que Render asigna, o usa 5000 por defecto localmente
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
