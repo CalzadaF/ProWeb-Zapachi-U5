f0dea66d3043f131ba420be7ddc0b9e91e3db4f0
CalzadaF <a21490530@itmexicali.edu.mx>
2025-05-26 11:44:26 -0700

    comentarios

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from app import create_app
 app= create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) # aqui se ejecuta la aplicacion, si se pone debug=True, se recarga automaticamente al hacer cambios en el codigo
 
--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,20 @@ from app.routes.auth_routes import auth_bp
 from app.routes.editor_routes import editor_bp
 from app.routes.admin_routes import admin_bp
 
-def create_app():
+def create_app(): # Esta funcion crea la aplicacion Flask y la configura, la configuracion se base en las extensiones y las rutas que se importan
     app = Flask(__name__)
-    app.secret_key = 'zapachi_puro_ct'
+    app.secret_key = 'zapachi_puro_ct' # Clave secreta para la aplicacion, se usa para firmar cookies y sesiones, si se cambia la clave, se invalidan las sesiones existentes
 
-    app.config['MYSQL_HOST'] = 'Zapachi.mysql.pythonanywhere-services.com'
-    app.config['MYSQL_USER'] = 'Zapachi'
-    app.config['MYSQL_PASSWORD'] = 'Bash2149'
-    app.config['MYSQL_DB'] = 'Zapachi$Zproductos_db'
+    app.config['MYSQL_HOST'] = 'Zapachi.mysql.pythonanywhere-services.com' # Host del servidor MySQL, en este caso es el servidor de PythonAnywhere
+    app.config['MYSQL_USER'] = 'Zapachi' # Usuario de la base de datos MySQL, en este caso es el usuario de PythonAnywhere
+    app.config['MYSQL_PASSWORD'] = 'Bash2149' # Contraseña del usuario de la base de datos MySQL, en este caso es la contraseña de PythonAnywhere
+    app.config['MYSQL_DB'] = 'Zapachi$Zproductos_db' # Nombre de la base de datos MySQL, en este caso es la base de datos de PythonAnywhere
 
-    mysql.init_app(app)
+    mysql.init_app(app) # Inicializa la extension MySQL con la aplicacion Flask
 
-    app.register_blueprint(public_bp)
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(editor_bp)
-    app.register_blueprint(admin_bp)
+    app.register_blueprint(public_bp) # Registra los blueprints de las rutas publicas, de autenticacion, de editor y de administrador
+    app.register_blueprint(auth_bp) # Registra el blueprint de las rutas de autenticacion
+    app.register_blueprint(editor_bp) # Registra el blueprint de las rutas del editor
+    app.register_blueprint(admin_bp) # Registra el blueprint de las rutas del administrador
 
-    return app
+    return app 
